f0f66ca259940a5a480255b0b13a9c1f753428d1
Eldor Azimov <eldorazimov37@gmail.com>
2022-12-12 10:25:49 +0900

    Programmatic (Imperative) Navigarion and Prompt

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         <Route path="/new-quote">
           <NewQuote />
         </Route>
-        <Route>
+        <Route path="*">
           <NotFound />
         </Route>
       </Switch>
